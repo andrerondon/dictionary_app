a4e4566f584d6804167abac6e2bb847f48b8f9db
andre-rondon <rondon@estudiorondon.com.br>
2020-03-09 10:01:43 -0700

    this is the labrary running

--- dictionaryApp.js
+++ dictionaryApp.js
@@ -2,13 +2,7 @@
 
 // HEY GUYS PLEASE DOESN'T CHANGE THE NAME OF THE PARAMTERS PLEASE -- ANDRE
 
-//
 
-
-
-
-
-////////////////////////////////////this code was previously here////////////////////////////////////////
 
 const http = require("http");
 const url = require('url');
@@ -18,58 +12,81 @@ const fs = require('fs');
 const server = http.createServer((request, response) => { 
   const {pathname, query} = url.parse(request.url, true); 
   console.log(query)
-
-  response.writeHead(200, {'content-type': 'text/html'});
-
-  let body = '';
-  request.on('data', (chunk) =>{
-    body += chunk;
-    console.log(body);
-  })
-  request.on('end', () =>{
-    request.body = qs.parse(body);
-    const resource = `${request.method} ${pathname}`
-    if (resource === 'GET /style') {
-      response.writeHead(200, {'content-type': 'text/text'});
-      response.write('h1 {color: green; font-size: 50px}')
-    }
-    
-    if (resource === 'GET /'){
-      response.write('<html><body><h1>Welcome to Dictionary</h1></body></html>');
+  let findVocabulary = query.word;
+  let arr = [];
+  console.log(findVocabulary);
+  
+fs.readFile('labrary.txt', 'utf8', (err, data) =>{
+    if (err){
+      console.log(err);
       
-      return response.end()
+    } else{
+      response.writeHead(200, {'content-type': 'text/html'});
+      response.write('<html><body><h1>Welcome to Dictionary</h1></body></html>');
+      let vocabulary = data.split('\n');
+      console.log(vocabulary);
+      
+      for (let i = 0; i < vocabulary.length; i ++){
+        if (vocabulary[i].startsWith(findVocabulary)){
+          arr.push(vocabulary[i]);  
+        } 
+     }
+     response.write(`<html><body><h1>${arr[0]}</h1></body></html>`);
+     return response.end();
     }
-    if(resource === 'GET /word'){
-      let word = resource.substr(5);
-      for(let x = 0; x < 10; x++){
-          word = word.replace('%20',' ');
-       }
-       fs.readFile('./labrary.txt','utf8',function(err,data){
-           if(err){
-               console.log(err);
-           }
-           let count = 0;
-           let dictionaryConvertedIntoArrOfStrings = data.split('\n');
-           for (let x = 0; x < dictionaryConvertedIntoArrOfStrings.length; x++){
-               if(dictionaryConvertedIntoArrOfStrings[x].startsWith(word)){
-                   response.write(`<html><body><h1>definition :-</h1><p>${dictionaryConvertedIntoArrOfStrings[x].substr(word.length+2)}</p></body></html>`);
-                   count++;
-               }            
-           }
-           if(count === 0){
-               response.write('<html><body><h1>Sorry your word was not found</h1></body></html>');
-           }   
-           return response.end()    
-       })    
-     
-    }
+    return response.end()
 
+    
+    })
 
-  })
-  
-  
-  
 });
+
+    // const resource = `${request.method} ${pathname}`
+
+
+    // if (resource === 'GET /style') {
+    //   response.writeHead(200, {'content-type': 'text/text'});
+    //   response.write('h1 {color: green; font-size: 50px}')
+    // }
+    
+    // if (resource === 'GET /'){
+    //   response.write('<html><body><h1>Welcome to Dictionary</h1></body></html>');
+    //   return response.end()
+    // }
+    // if(resource === 'GET ?/word'){
+        
+     
+    // }
+
+    // let word = resource.substr(5);
+    // for(let x = 0; x < 10; x++){
+    //     word = word.replace('%20',' ');
+    //  }
+    //  fs.readFile('./labrary.txt','utf8',function(err,data){
+    //      if(err){
+    //          console.log(err);
+    //      }
+    //      console.log(" IM HEREEEEEEEEEEEEEEEEEEEEEE");
+         
+    //      let count = 0;
+    //      let dictionaryConvertedIntoArrOfStrings = data.split('\n');
+    //      for (let x = 0; x < dictionaryConvertedIntoArrOfStrings.length; x++){
+    //          if(dictionaryConvertedIntoArrOfStrings[x].startsWith(word)){
+    //              response.write(`<html><body><h1>definition :-</h1><p>${dictionaryConvertedIntoArrOfStrings[x].substr(word.length+2)}</p></body></html>`);
+    //              count++;
+    //          }            
+    //      }
+    //      if(count === 0){
+    //          response.write('<html><body><h1>Sorry your word was not found</h1></body></html>');
+    //      }   
+    //      return response.end()    
+    //  })   
+
+
+  
+  
+  
+
 
 
 const PORT = 5000;
@@ -79,53 +96,4 @@ const DOMAIN = 'localhost';
     console.log(`Server Dictionary App running  ${DOMAIN}:${PORT}`);
   })
 
-///////////////////////////////////////this code was previously here/////////////////////////////////////
 
-
-
-
-
-
-
-///////////////////////////////////////////sandeep's version////////////////////////////////////////////////
-// const http = require('http');
-// const url = require('url');
-// const fs = require('fs');
-// const server = http.createServer(function(req,res){
-    
-//     let {pathname,query} = url.parse(req.url); 
-//     if(typeof(query)==='object'){
-//         res.write('<html><body><h1>Welcome to Dictionary</h1></body></html>');
-//     }
-//     if(typeof(query) === 'string'){
-//         let word = query.substr(5);
-//         for(let x=0;x<10;x++){
-//             word =  word.replace('%20',' ');
-//          }
-//          fs.readFile('./labrary.txt','utf8',function(err,data){
-//              if(err){
-//                  console.log(err);
-//              }
-//              let count = 0;
-//              let dictionaryConvertedIntoArrOfStrings = data.split('\n');
-//              for (let x=0;x<dictionaryConvertedIntoArrOfStrings.length;x++){
-//                  if(dictionaryConvertedIntoArrOfStrings[x].startsWith(word)){
-//                      res.write(`<html><body><h1>definition :-</h1><p>${dictionaryConvertedIntoArrOfStrings[x].substr(word.length+2)}</p></body></html>`);
-//                      count++;
-//                  }            
-//              }
-//              if(count === 0){
-//                  res.write('<html><body><h1>Sorry your word was not found</h1></body></html>');
-//              }       
-//          })    
-//     }
-
-    
-    
-// });
-// const PORT = 9876;
-// const DOMAIN = '127.0.0.1';
-// server.listen(PORT,DOMAIN,function(){
-//     console.log('server running');
-// })
-///////////////////////////////////////////sandeep's version//////////////////////////////////////////////
